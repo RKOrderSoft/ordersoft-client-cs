6396cdcb716c22f28dd584dd8e14e33b903d9ab5
Eddie Qi <eddieqi128@gmail.com>
2018-06-30 12:36:46 +1000

    still tryna make everything work..

--- Client.cs
+++ Client.cs
@@ -5,53 +5,60 @@ using System.IO;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace OrderSoft {
+	public class Response {
+		public string ordersoft_version { get; set; }
+		public string reason { get; set; }
+	}
+
 	public class OSClient {
 		public const string CLIENT_ID = "dotnet";
 
 		private static readonly HttpClient httpClient = new HttpClient();
-		private Uri endpoint;
 		private string sessionId;
+		private Uri endpoint;
 
-		public OSClient (string serverIP) {
-			Console.WriteLine(serverIP);
-			endpoint = new Uri(Path.Combine(serverIP, "api/"));
-			Console.WriteLine(endpoint.ToString());
-
-			isOrdersoftServer(endpoint);
+		public OSClient() {
+			httpClient.DefaultRequestHeaders.Add("client", CLIENT_ID);
 		}
 
-		/*public async void authenticate(string username, string password) {
-			// TODO
-			return;
-		}*/
+		public async void init(string newUrl) {
+			endpoint = new Uri(Path.Combine(newUrl, "api/"));
 
-		private async void isOrdersoftServer(Uri toCheck) {
-			// Check that IP is indeed that of an OrderSoft server
-			var vals = new Dictionary<string,string> {};
-			var response = await requestToServer("test", vals);
-			var responseText = await response.Content.ReadAsStringAsync();
-			Console.WriteLine(responseText);
+			var vals = new Dictionary<string,bool> {};
+			vals.Add("test", true);
+			var response = await sendRequest("test", vals);
+			var yes = response.StatusCode == HttpStatusCode.OK;
+			Console.WriteLine(yes.ToString() + response.StatusCode.ToString());
+
+			string responseContent = await response.Content.ReadAsStringAsync();
+			Console.WriteLine(responseContent);
+			Response initResponse = JsonConvert.DeserializeObject<Response>(responseContent);
+			Console.WriteLine(initResponse.ordersoft_version);
 		}
 
-		private Task<HttpResponseMessage> requestToServer (String relativeUrl, Dictionary<string,string> bodyVals) {
+		private Task<HttpResponseMessage> sendRequest (String relativeUrl, 
+		  Dictionary<string,bool> bodyVals) {
 			var urlToPost = new Uri(endpoint, relativeUrl);
 
-			bodyVals.Add("client", CLIENT_ID);
+			// Add sessionId if initialised, else warn
+			Console.WriteLine(httpClient.DefaultRequestHeaders);
 			if (sessionId == null) {
-				Console.WriteLine("Attempting to POST " + relativeUrl + " with no sessionId");
+				Console.Write("Attempting to POST ");
+				Console.Write(relativeUrl);
+				Console.WriteLine(" with no sessionId");
 			} else {
-				bodyVals.Add("sessionId", sessionId);
+				httpClient.DefaultRequestHeaders.Add("sessionId", sessionId);
 			}
-			Console.WriteLine(bodyVals);
-			Console.WriteLine(JsonConvert.SerializeObject(bodyVals));
 
 			// Convert dict to JSON, StringContent
-			var bodyContent = new StringContent(JsonConvert.SerializeObject(bodyVals), Encoding.UTF8, "application/json");
+			var bodyJSON = JsonConvert.SerializeObject(bodyVals);
+			var bodyContent = new StringContent(bodyJSON, Encoding.UTF8, 
+			  "application/json");
 
-			return httpClient.PostAsync(urlToPost, bodyContent);
-			//return response.Content.ReadAsStringAsync();
+			return httpClient.PostAsync(urlToPost, bodyContent); // TODO handle get/delete
 		}
 	}
 }
